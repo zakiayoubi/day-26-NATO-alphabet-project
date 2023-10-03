9586bd3277328f680b72c9be7b3894bfb790f4e0
ayoubim <ayoubim@berea.edu>
2023-10-02 20:35:10 -0400

    Implemented Error Handling

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ import pandas
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row["letter"]: row["code"] for (index, row) in nato_df.iterrows()}
+print(nato_dict)
 
-user_input = input("Enter a word: ").upper()
 
-words_list = [nato_dict[letter] for letter in user_input]
-print(words_list)
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        words_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Please only enter letters. ")
+        generate_phonetic()
+    else:
+        print(words_list)
+
+
+generate_phonetic()
+
+
